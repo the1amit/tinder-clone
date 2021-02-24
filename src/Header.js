7bb571223763771f73543b5c43c9aa0b9344d119
the1amit <the_amit@live.com>
2021-02-23 19:35:16 -0500

    logo updated

--- src/Header.js
+++ src/Header.js
@@ -1,45 +1,39 @@
-import React from 'react'
-import './Header.css'
-import PersonIcon from '@material-ui/icons/Person';
-import IconButton from '@material-ui/core/IconButton';
-import ForumIcon from '@material-ui/icons/Forum';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { Link, useHistory } from 'react-router-dom';
-
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { Link, useHistory } from "react-router-dom";
 
 function Header({ backButton }) {
-    const history = useHistory();
-    return (
-        <div className="header">
-            {backButton ? (
+  const history = useHistory();
+  return (
+    <div className="header">
+      {backButton ? (
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      )}
 
-                <IconButton onClick={() => history.replace(backButton)}>
-                    <ArrowBackIosIcon fontSize="large" className="header__icon" />
-                </IconButton>
-
-            ) : (
-                    <IconButton>
-                        <PersonIcon fontSize="large" className="header__icon" />
-                    </IconButton>
-                )}
-
-            <Link to="/">
-                <img
-                    className="header__logo"
-                    src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-                    alt="Tinder Logo"
-                />
-            </Link>
-            <Link to="/chat">
-                <IconButton>
-                    <ForumIcon fontSize="large" className="header__icon" />
-                </IconButton>
-            </Link>
-
-
-
-        </div>
-    )
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
+          alt="Tinder Logo"
+        />
+      </Link>
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon fontSize="large" className="header__icon" />
+        </IconButton>
+      </Link>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
